7b2902303480a7898365efdfcb684587ea71c402
nathandg <nathan.silva@padolabs.com.br>
2023-03-03 22:13:29 -0300

    :construction: new backend folder

--- app/redux/store.tsx
+++
@@ -1,9 +0,0 @@
-import {createStore, combineReducers} from 'redux';
-import AccountReducer from './reducer/AccountReducer';
-
-const rootReducer = combineReducers({
-  account: AccountReducer,
-});
-
-const store = createStore(rootReducer);
-export default store;
--- app/types/Connection.tsx
+++
@@ -1,7 +0,0 @@
-interface Connection {
-  id: string;
-  name: string;
-  mongo_uri: string;
-}
-
-export default Connection;
